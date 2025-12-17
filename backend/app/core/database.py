6d4973797a951c984cf1d8df3ee27e76a6bd8ac3
klin2686 <kennethlin2686@gmail.com>
2025-12-16 22:58:31 -0800

    feat: add database connection setup and add default development database url to .env.example

---
+++ backend/app/core/database.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import Session, create_engine
+
+from app.core.config import settings
+
+engine = create_engine(settings.database_url)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
