ec549bf43114b6ca8333f2686d1629e7e4ebf4e8
klin2686 <kennethlin2686@gmail.com>
2025-12-23 02:30:17 -0800

    style: correct formatting in python constants schema

--- shared/constants_schema.py
+++ shared/constants_schema.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 
+
 class CamelCaseModel(BaseModel):
     model_config = ConfigDict(
         alias_generator=to_camel,
         populate_by_name=True,
-        extra='forbid'
+        extra='forbid',
     )
 
 
@@ -26,4 +27,3 @@ class ResponseConstants(CamelCaseModel):
 class Constants(CamelCaseModel):
     job_description_constants: JobDescriptionConstants
     response_constants: ResponseConstants
-
